94952d15ac9f8deb74e231faf68337bd7039b6c3
monikdev <monik_ita2006@yahoo.com.br>
2019-08-01 08:26:51 -0300

    Projeto criado

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author monik
+ *
+ */
+module teste {
+}
---
+++ src/testegithub.java
@@ -0,0 +1,9 @@
+
+public class testegithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
